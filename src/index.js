98dde39ecf05b345320a96f8c57ba40c42eeffe5
Diego F. Goberna <github@feiss.be>
2020-06-09 20:55:57 +0200

    added tile-based set, collisions with set, camera following

--- src/index.js
+++ src/index.js
@@ -27,13 +27,13 @@ for (let i = 0; i < textureURLs.length; i++) {
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0.7, 1.5);
+camera.position.set(0, 0.6, 1.1);
 camera.lookAt(0,0.1,0);
 
 var renderer = new THREE.WebGLRenderer();
 renderer.shadowMap.enabled = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.setClearColor(0x88aacc)
-//renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
 document.body.appendChild(renderer.domElement);
 
@@ -51,7 +51,6 @@ function debugStr(str) {
   debug.innerHTML += str + '<br>';
 }
 
-
 // window resize handler
 
 window.addEventListener('resize', onResize);
@@ -62,6 +61,9 @@ function onResize() {
 };
 onResize();
 
+
+var john = new THREE.Mesh(new THREE.SphereBufferGeometry(0.01));
+//scene.add(john);
 
 // controls
 
@@ -99,15 +101,18 @@ function onDesktopInputEvent(ev) {
     case 69: // e
       controls.jump = pressed;
       break;
+    case 82: // r, reset
+      reset();
+      break;
   }
 }
 
 // scene
 
-var light = new THREE.HemisphereLight(0x6688ff, 0x227722, 2);
+var light = new THREE.HemisphereLight(0x6688ff, 0x227722, 1);
 scene.add( light );
 var light = new THREE.DirectionalLight(0xaaaaaa,1);
-light.position.set(0.2, 1.7, -0.7);
+light.position.set(0.2, 1.7, 0.7);
 light.castShadow = true;
 light.shadow.camera.top = 10;
 light.shadow.camera.bottom = -10;
@@ -116,12 +121,15 @@ light.shadow.camera.left = -10;
 light.shadow.mapSize.set(4096, 4096);
 scene.add(light);
 
+var tiles = null;
+var arrowHelper = null;
+/* floor
 var geometry = new THREE.PlaneBufferGeometry(4, 4).rotateX(-Math.PI / 2);
 var material = new THREE.MeshLambertMaterial({ color: 0x21351d });
 var floor = new THREE.Mesh(geometry, material);
 floor.receiveShadow = true;
 scene.add(floor);
-
+*/
 
 // foxr object
 var foxr = {
@@ -132,14 +140,16 @@ var foxr = {
 
   speed: new THREE.Vector2(), // speed vector on floor
   jump: 0, // jump speed
-  onAir: false, // is jumping or falling
+  onAir: true, // is jumping or falling
+  floorPoint: new THREE.Vector3(), // point in his feet, to calculate colision with the floor
+  bellyPoint: new THREE.Vector3(), // point in his bellybutton, to calculate colision with the walls
 
   // constants
-  SCALE: 0.12, // global size
-  ACCELERATION: 1,
+  ACCELERATION: 0.15,
   FRICTION: 0.9,
-  MAX_SPEED: 0.3,
-  JUMP_SPEED: 0.3,
+  MAX_SPEED: 0.03,
+  JUMP_SPEED: 0.03,
+  OFFSET_Y: 0.046169
 };
 
 window.foxr = foxr;
@@ -163,7 +173,7 @@ foxr.playAnim = (anim) => {
   foxr.currentAnim = foxr.anims[anim];
 }
 
-// load and setup foxr
+// load and setup scene
 
 new GLTFLoader().load('assets/foxr.glb', gltf => {
   var foxrMesh = gltf.scene.getObjectByName('foxr');
@@ -172,6 +182,8 @@ new GLTFLoader().load('assets/foxr.glb', gltf => {
     emissiveMap: textures['foxr_diff.png'],
     skinning: true
   })
+
+  foxrMesh.frustumCulled = false;
   foxrMesh.castShadow = true;
 
   var headsetMesh = gltf.scene.getObjectByName('foxr_headset');
@@ -183,8 +195,12 @@ new GLTFLoader().load('assets/foxr.glb', gltf => {
     skinning: true
   })
 
+  headsetMesh.frustumCulled = false;
+
+  //foxrMesh.material.map.encoding = THREE.LinearEncoding;
+
+
   foxr.object3D = gltf.scene.getObjectByName('Armature');
-  gltf.scene.scale.set(foxr.SCALE, foxr.SCALE, foxr.SCALE);
   scene.add(gltf.scene);
 
   // animations
@@ -214,36 +230,54 @@ new GLTFLoader().load('assets/foxr.glb', gltf => {
 
   foxr.mixer = mixer;
   foxr.playAnim('idle');
+
+
+  // floor
+
+  tiles = gltf.scene.getObjectByName('tiles').children;
+  for (var i = 0; i < tiles.length; i++) {
+    //dtiles[i].castShadow = true;
+    tiles[i].receiveShadow = true;
+  }
+
   clock.start();
   animate();
 });
 
 
 const LOOK_AT_MUL = new THREE.Vector2(500, 500);
-const FLOOR_LEVEL = 0.3772455155849457;
+const FLOOR_LEVEL = 0.110539;
+
+function reset() {
+  foxr.speed.set(0, 0);
+  foxr.jump = 0;
+  foxr.onAir = true;
+  foxr.object3D.position.set(0, 0.156169, 0);
+}
 
 function update(time, dt) {
-  if (controls.left) { foxr.speed.x -= foxr.ACCELERATION * dt; }
-  if (controls.right){ foxr.speed.x += foxr.ACCELERATION * dt; }
-  if (controls.up)   { foxr.speed.y -= foxr.ACCELERATION * dt; }
-  if (controls.down) { foxr.speed.y += foxr.ACCELERATION * dt; }
+  const acceleration = foxr.ACCELERATION * (foxr.onAir ? 0.8 : 1);
+  if (controls.left) { foxr.speed.x -= acceleration * dt; }
+  if (controls.right){ foxr.speed.x += acceleration * dt; }
+  if (controls.up)   { foxr.speed.y -= acceleration * dt; }
+  if (controls.down) { foxr.speed.y += acceleration * dt; }
   if (controls.jump && !foxr.onAir){
     foxr.jump = foxr.JUMP_SPEED;
     foxr.onAir = true;
   }
 
-  const speed = foxr.speed.length();
+  var speed = foxr.speed.length();
 
   if (foxr.onAir){
     foxr.playAnim('jump');
   }
-  else if (speed > 0.02){
+  else if (speed > 0.005){
     foxr.playAnim('run');
   } else {
     foxr.playAnim('idle');
   }
 
-  foxr.anims.run.timeScale = Math.max(0.5, speed * 7);
+  foxr.anims.run.timeScale = Math.max(0.5, speed * 70);
 
   if (speed > foxr.MAX_SPEED){
     foxr.speed.normalize().multiplyScalar(foxr.MAX_SPEED);
@@ -256,26 +290,64 @@ function update(time, dt) {
   foxr.speed.x *= foxr.FRICTION;
   foxr.speed.y *= foxr.FRICTION;
 
-  foxr.jump -= 0.8 * dt;
+  foxr.jump -= 0.08 * dt;
 
   // apply to object3d
   foxr.object3D.position.x += foxr.speed.x;
   foxr.object3D.position.y += foxr.jump;
   foxr.object3D.position.z += foxr.speed.y;
 
-  if (foxr.object3D.position.y <= FLOOR_LEVEL) {
-    foxr.object3D.position.y = FLOOR_LEVEL;
-    foxr.onAir = false;
+
+  // collision with floor
+  foxr.onAir = true;
+  foxr.floorPoint.set(
+    foxr.object3D.position.x,
+    foxr.object3D.position.y - foxr.OFFSET_Y,
+    foxr.object3D.position.z
+    );
+
+  foxr.bellyPoint.set(
+    foxr.object3D.position.x + foxr.speed.x * 2,
+    foxr.object3D.position.y,
+    foxr.object3D.position.z + foxr.speed.y * 2
+    );
+
+  for (var i = 0; i < tiles.length; i++) {
+    const bb = tiles[i].geometry.boundingBox;
+    if (bb.containsPoint(foxr.bellyPoint)){
+      foxr.object3D.position.x -= foxr.speed.x * 1.1;
+      foxr.object3D.position.z -= foxr.speed.y * 1.1;
+      foxr.speed.set(0, 0);
+      speed = 0;
+    }
+    else if (bb.containsPoint(foxr.floorPoint)){
+      foxr.object3D.position.y = bb.max.y + foxr.OFFSET_Y;
+      foxr.jump = 0;
+      foxr.onAir = false;
+      break;
+    }
   }
 
-
-  if (speed > 0.01) {
+  if (speed > 0.001) {
     const lookAt = foxr.speed.clone();
     lookAt.normalize().multiply(LOOK_AT_MUL);
     foxr.object3D.lookAt(lookAt.x, 0, lookAt.y);
   }
 
-  if (foxr.speed.length() < 0.001) { foxr.speed.set(0, 0); }
+  if (foxr.speed.length() < 0.0001) { foxr.speed.set(0, 0); }
+
+  camera.position.set(
+    foxr.object3D.position.x,
+    foxr.object3D.position.y * 0.5 + 0.5,
+    foxr.object3D.position.z + 1.2,
+    );
+
+  camera.lookAt(
+    foxr.object3D.position.x,
+    foxr.object3D.position.y * 0.5,
+    foxr.object3D.position.z
+    );
+
 }
 
 // main loop
